a34f03f8ebe71ed39f02f6574ce5ea3ff7df39c6
giovannipacelli2 <giovannipacelli2@gmail.com>
2022-07-01 23:33:32 +0200

    Creazione funzioni specifiche + controllo numeri negativi

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,37 @@ container.onmousedown = function () { return false };
 
 function buttonEvent( e ) {
 	let button = e.target;
+
+	check(button, counter);
+
+	function check (button, counter) {
+		if (!button) return;
+		if (!button.classList.contains("button")) return;
+
+		if ( button.id === "increase" ) {
+			increase(counter);
+		}
+		if ( button.id === "decrease" ) {
+			decrease(counter);
+		}
+
+	}
 	
-	if (!button) return;
-	if (!button.classList.contains("button")) return;
-	
-	e.preventDefault();
-	
-	if ( button.id === "increase" ) {
+	function increase (counter) {
 		let result = Number( counter.textContent );
 		counter.textContent = ++result;
 	}
-	if ( button.id === "decrease" ) {
+
+	function decrease (counter) {
 		let result = Number( counter.textContent );
-		counter.textContent = --result;
+		result--;
+		if ( result >= 0 ) {
+			counter.textContent = result;
+		}
+		
 	}
+	
+	e.preventDefault();
+	
+	
 }
